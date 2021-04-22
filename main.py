1d7bcc2ca819b7dc5c8d2b0364edf031d7cab6c2
Victor Poutot <v.poutot@gmail.com>
2021-04-22 08:47:57 +0200

    suppression des derniers sys.path

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-import sys
 from view.view import menu
-
-sys.path.append("view")
-sys.path.append("controller")
-sys.path.append("model")
 
 
 def main():
